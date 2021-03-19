27276f4b0aed173c39513a3eb4f938c1ea2c0818
billduque34@gmail.com <billduque34@gmail.com>
2021-03-19 21:56:02 +0800

    Piano files added

---
+++ script.js
@@ -0,0 +1,19 @@
+const audio = document.querySelector('audio');
+const keys = ['key-c','key-d','key-e','key-f','key-g','key-a','key-b','key-cs'];
+
+const notes = [];
+keys.forEach(function(key) {
+    notes.push(document.getElementById(key));
+});
+
+for(let i=0; i<notes.length;i++) {
+    notes[i].value = keys[i];
+}
+
+function keydown(event) {
+    audio.src = '/resources/'+ event.target.value +'.mp3';
+}
+
+notes.forEach(function(note) {
+    note.addEventListener('mousedown',keydown);
+});
